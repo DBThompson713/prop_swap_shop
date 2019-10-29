7db0dc0926684d3fd9f6ae51375674a778e9a7df
DB Thompson <dbthompson.au@gmail.com>
2019-10-30 10:29:53 +1100

    linked index, plant and profile pages, set adding a plant and profiles behind authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   resources :plants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # home#index
+  # root :to => “pages#home”
+
+  get "/", to: "pages#home", as: "root"
+  # get "/plants", to: "plants#index", as: "plants"
+  # post "/plants", to: "plants#create"
+  # get "/plants/new", to: "plants#new", as: "new_plant"
+  # get "/plants/:id", to: "plants#show", as: "plant"
+  # patch "/plants/:id", to: "plants#update"
+  # get "/plants/:id/edit", to: "plants#edit", as: "edit_plant"
+  # get "/payment/success", to: "payments#success", as: "success_payment"
+  # post "/payment/webhook", to:"payments#webhook", as: "webhook_payment"
 end
